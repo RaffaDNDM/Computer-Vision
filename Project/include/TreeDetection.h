8429bc35ee2ddeae8c9c3daa8055f1cf56693437
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-06-23 22:56:31 +0200

    Created folder for project.

---
+++ Project/include/TreeDetection.h
@@ -0,0 +1,32 @@
+/**
+	@file TreeDetection.h
+	@brief Header of source file with main.
+	@author Raffaele Di Nardo DI Maio 1204879
+*/
+
+#ifndef TREE_DETECTION
+#define TREE_DETECTION
+
+/*
+#include <memory>
+#include <iostream>
+#include <limits>
+#include <thread>
+#include <mutex>
+*/
+
+/*
+#include <opencv2/core.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/stitching.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/video/tracking.hpp>
+*/
+
+#define LINE "-----------------------------------------------\n"
+
+#endif
---
+++ Project/src/TreeDetection.cpp
@@ -0,0 +1,12 @@
+/**
+	@file TreeDetection.cpp
+	@brief Source file with main.
+	@author Raffaele Di Nardo DI Maio 1204879
+*/
+
+#include "TreeDetection.h"
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
